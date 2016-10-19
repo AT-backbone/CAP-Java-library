428924f08e56f8b80c7af3796e6fe9e00b6c97eb
Niklas Spanring <n.spanring@backbone.co.at>
2016-10-19 16:33:27 +0200

    Init Repo
    Signed-off-by: Niklas Spanring <n.spanring@backbone.co.at>

---
+++ src/cap/php/library/CAP_PHP_Library.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cap.php.library;
+
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author nspan
+ */
+public class CAP_PHP_Library extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+                
+                CapProcessor cap = new CapProcessor("", "");
+                cap.makeTestCAP(true);
+                String test = cap.buildCap(0);
+                try {
+                    test+= cap.saveCap("test.cap.xml", 0);
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(CAP_PHP_Library.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (UnsupportedEncodingException ex) {
+                    Logger.getLogger(CAP_PHP_Library.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                System.out.println(test);
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
